c6a77526f1e8d063706ca5f1fb46729b935230e9
Hector Reyes <damianreyes52@gmail.com>
2024-02-27 17:14:27 -0800

    Implemented Firebase configuration

--- pcbuddy_website/assets/js/login_signup.js
+++ pcbuddy_website/assets/js/login_signup.js
@@ -23,6 +23,30 @@ document.getElementById('darkMode').addEventListener('click', function() {
 });
 
 
+// // Firebase configuration
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyANdcsuyJx9lcQ2sQrFoPLLfNVmztCgNys",
+//   authDomain: "aipcbuddy.firebaseapp.com",
+//   projectId: "aipcbuddy",
+//   storageBucket: "aipcbuddy.appspot.com",
+//   messagingSenderId: "322988785378",
+//   appId: "1:322988785378:web:838bcb9948db3273efe8bb",
+//   measurementId: "G-59MGYJGZ5C"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+
 
 // const signUpButton = document.getElementById('signUp');
 // const signInButton = document.getElementById('signIn');
