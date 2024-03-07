6c5402295252bedda47f48827508ea2b1ed7a0ea
Hamzah Qutob <hqutob.19@gmail.com>
2024-03-07 11:20:17 -0800

    recaptcha and accessories page template

--- pcbuddy_website/script.js
+++ pcbuddy_website/script.js
@@ -1,3 +1,4 @@
-function successCallback() {
-    debugger;
-}
+app.post('/submit-form', (req, res) => {
+    const recaptchaResponse = req.body['g-recaptcha-response'];
+    // Continue with verification...
+});
