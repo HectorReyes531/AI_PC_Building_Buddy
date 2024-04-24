35a12d72daaecfb19fb025a92280f094d4c716f1
Hector Reyes <damianreyes52@gmail.com>
2024-04-23 21:16:15 -0700

    .

--- pcbuddy_website/server.js
+++ pcbuddy_website/server.js
@@ -30,7 +30,7 @@ const server = http.createServer((req, res) => {
     }
 });
 
-const PORT = 8001;
+const PORT = 8002;
 server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
