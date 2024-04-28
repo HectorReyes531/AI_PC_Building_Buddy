11f08c4f20af72deefc47d10581b55cdee93fb33
ahoang54 <143464008+ahoang54@users.noreply.github.com>
2024-04-28 13:00:29 -0700

    populating tables

--- pcbuddy_website/assets/js/dbquery.js
+++ pcbuddy_website/assets/js/dbquery.js
@@ -5,6 +5,25 @@ document.getElementById("submitBtn").addEventListener("click", async function(){
     const url = `http://localhost:3000?part=${part}`;
     const response = await fetch(`${url}`);
     const data = await response.json();
-    console.log(typeof(data));
+    // console.log(typeof(data));
     console.log(data);
+    const table = document.getElementById("data_rep");
+    table.innerHTML = '';
+
+    // Creating a header for the table
+    const headerRow = table.insertRow();
+    Object.keys(data[0]).forEach(key=>{
+        const th = document.createElement('th');
+        th.textContent = key;
+        headerRow.appendChild(th);
+    })
+    // Populating the table
+    console.log("inserting cells");
+    data.forEach(entry =>{
+        const row = table.insertRow();
+        Object.values(entry).forEach(value =>{
+            const cell = row.insertCell();
+            cell.textContent = value;
+        })
+    })
 })
